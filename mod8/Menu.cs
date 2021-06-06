c6060235c69649a20d833a413e90fe536ba4cd80
Grindelwalt <Sulxj600s@gmail.com>
2021-06-06 14:14:23 +0300

    Вынос метода меню в отдельный класс

---
+++ mod8/Menu.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod8
+{
+    
+    class Menu
+    {
+        public void Print(string a)
+        {
+            Console.WriteLine(a);
+        }
+        public void MenuCompany()
+        {
+            Repository rep = new Repository();
+            char key = 'д';
+            Print("считать данные с XML или JSON? д/н");
+            key = Console.ReadKey(true).KeyChar;
+            if (char.ToLower(key) == 'д')
+            {
+                Print("Десерелизовать XML - 1");
+                Print("Десерелизовать JSON - 2");
+                int index = Convert.ToInt32(rep.ExceptionsInt());
+                switch (index)
+                {
+                    case 1:
+                        rep.XMLRead();
+                        break;
+                    case 2:
+                        rep.JSONRead();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Print("Создание первого департамента");
+                rep.RecordDep();
+                Print("Добавления сотрудников");
+                rep.RecordEmployee();
+            }
+
+            do
+            {
+                Print("Действия в отделе кадров производства");
+                Print("");
+                Print("Добавить департамент - 1");
+                Print("Добавить сотрудника - 2");
+                Print("Уволить сотрудника - 3");
+                Print("Редактировать данные сотрудника - 4");
+                Print("Редактировать департамент - 10");
+                Print("Расформировать департамент - 11");
+                Print("Перевезти сотрудника - 12");
+                Print("Сортировка сотруднирков - 13");
+                Print("");
+                Print("Работа с XML и JSON");
+                Print("");
+                Print("Серелизовать XML - 5");
+                Print("Серелизовать JSON - 6");
+                Print("Десерелизовать XML - 7");
+                Print("Десерелизовать JSON - 8");
+                Print("Печать - 9");
+
+                int index = Convert.ToInt32(rep.ExceptionsInt());
+                switch (index)
+                {
+                    case 1:
+                        rep.RecordDep();
+                        break;
+                    case 2:
+                        Print("Существующие департаменты");
+                        rep.PrintDepartment();
+                        rep.RecordEmployee();
+                        break;
+                    case 3:
+                        rep.PrintEmployee();
+                        Print("ID увольняемого сотрудника");
+                        rep.DismissEmployee();
+                        break;
+                    case 4:
+                        rep.PrintEmployee();
+                        Print("Выберите ID редактируемого сотрудника:");
+                        rep.EditEmployee(Convert.ToInt32(Console.Read()));
+                        break;
+                    case 5:
+                        rep.XMLRec();
+                        break;
+                    case 6:
+                        rep.JSONRec();
+                        break;
+                    case 7:
+                        rep.XMLRead();
+                        break;
+                    case 8:
+                        rep.JSONRead();
+                        break;
+                    case 9:
+                        rep.PrintDepartment();
+                        rep.PrintEmployee();
+                        break;
+                    case 10:
+                        Print("ID департамента:");
+                        rep.EditDep(Convert.ToInt32(Console.Read()));
+                        break;
+                    case 11:
+                        rep.DeleteDep();
+                        break;
+                    case 12:
+                        rep.TransferEmployee();
+                        break;
+                    case 13:
+                        rep.SortWorker();
+                        break;
+                    default:
+                        break;
+                }
+
+                Print("Открыть меню? д/н");
+                key = Console.ReadKey(true).KeyChar;
+            } while (char.ToLower(key) == 'д');
+        }
+    }
+}
--- mod8/Program.cs
+++ mod8/Program.cs
@@ -10,26 +10,8 @@ namespace mod8
     {
         static void Main(string[] args)
         {
-            Repository rep = new Repository();
-            rep.Menu();
-            Console.ReadKey();
-
-            
-
+            Menu rep = new Menu();
+            rep.MenuCompany();
         }
-        //static void Test()
-        //{
-        //    string[] words = new string[10];
-        //    words = words.Select((word, index) =>
-        //    {
-        //        Console.Write(string.Format("Введите строку {0}: ", (index + 1)));
-        //        return Console.ReadLine();
-        //    }).ToArray();
-        //    string newStr = string.Join(Environment.NewLine, words.Select((word, index) =>
-        //        (index + 1).ToString() + ": " + word).ToArray());
-        //    Console.WriteLine("Массив строк: ");
-        //    Console.WriteLine(newStr);
-        //    Console.ReadKey(true);
-        //}
     }
 }
